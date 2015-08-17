4e7a4f3d851b59e1277e32092d5457843475ed56
Salman Gadit <mogadit@microsoft.com>
2015-08-16 23:19:59 -0700

    Removed redis dependency

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var redis = require("redis"),
-    client = redis.createClient();
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('admin_login');
 });
 
 router.get('/home/:id', function(req, res, next) {
-  client.get(req.params.id, function(err, reply){
-    if (err){
-      res.redirect("/admin/invalid");
-    }
-    
-    res.render('admin', {accessToken: reply});
-  });
+  res.render('admin');
 });
 
 router.get('/invalid', function(req, res, next) {
@@ -29,8 +20,6 @@ router.post('/login', function(req, res, next) {
   console.log("Verifying " + name);
   if (name == "Salman Gadit" || name == "Sana Razzak"){
     console.log("Valid!")
-    
-    client.set(req.body.id, req.body.accessToken, redis.print);
     
     // valid credentials
     res.send({render: "/admin/home"});
